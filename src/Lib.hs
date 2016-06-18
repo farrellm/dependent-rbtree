7ea5f9b4026508b9287efd30c73e61567734d5c5
Matthew Farrell <farrellm@alum.mit.edu>
2016-06-17 22:59:37 -0400

    add QuickCheck

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,10 +10,15 @@ import Page2 ()
 import Page3 ()
 import qualified Page4.Page4 as RB
 
+deepCheck :: Testable prop => prop -> IO ()
+deepCheck = quickCheckWith stdArgs { maxSuccess = 1000, maxSize = 1000 }
+
 prop_sort :: Property
 prop_sort = forAll (listOf $ elements ([1..1000] :: [Int])) $ \xs -> sort xs == toList (rb xs)
   where rb :: [Int] -> RB.Tree Int
         rb = foldl' RB.insert RB.empty
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  deepCheck prop_sort
+  putStrLn "someFunc"
